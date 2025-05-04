b5b2e37774eab8dc4a5660b18dca5cde51d3ff72
chelsiabai <84818431+chelsiabai@users.noreply.github.com>
2025-05-04 13:17:16 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_X3wuWV2WZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5j9o82eood4u6a8fkmdf7b8tn4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
